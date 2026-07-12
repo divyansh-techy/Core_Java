d12f35397ffe3df2bf51d9c74a78eea79f554208
Divyansh Upadhyay <divyanshupadhyay05@gmail.com>
2026-07-12 12:08:25 +0530

    Java intro and print program

---
+++ java/InstaGreeting/src/InstaWelcome.java
@@ -0,0 +1,19 @@
+
+// This is the class declaration
+public class InstaWelcome {
+    // Main method - program starts here
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        // Print welcome message
+		System.out.println("Welcome to Instagram Java Edition!");
+		
+		//favorite app
+		System.out.println("Favorite App: Instagram");
+		
+		//my name
+		System.out.println("Name: Upadhyay Divyansh");
+
+	}
+
+}
