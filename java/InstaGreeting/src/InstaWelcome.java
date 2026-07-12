c5fc432730b2a4c1104fa8c431a234fea21a44aa
Divyansh Upadhyay <divyanshupadhyay05@gmail.com>
2026-07-12 12:09:29 +0530

    java Intro and Print Program

--- java/InstaGreeting/src/InstaWelcome.java
+++ java/InstaGreeting/src/InstaWelcome.java
@@ -1,4 +1,10 @@
 
+//JDK: basically JDK is a kit that provides all nasseccery things to develop java program.
+//JRE: JRE provides envierment to run the JAVA program.
+//JVM: JVM is that part that actually executes the Java code on the computer.
+
+
+
 // This is the class declaration
 public class InstaWelcome {
     // Main method - program starts here
